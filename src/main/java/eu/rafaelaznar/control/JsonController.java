51e2fd3a38fd7c2784c95dc83f2e8a89940e6998
a022583952e <a022583952e@S09-PC14>
2017-11-17 08:13:27 +0100

    cambiado login

--- src/main/java/eu/rafaelaznar/control/JsonController.java
+++ src/main/java/eu/rafaelaznar/control/JsonController.java
@@ -81,7 +81,7 @@ public class JsonController extends HttpServlet {
                 out.println("<head><title>Trolleyes server</title><link rel=\"shortcut icon\" href=\"favicon.ico\" type=\"image/x-icon\"></head>");
                 out.println("<body style=\"background: url(trolleyes400.png) no-repeat center center fixed;\">");
                 out.println("<h1>Welcome to trolleyes server</h1><h2>Servlet controller json listening at " + InetAddress.getLocalHost().getHostAddress() + ":" + request.getLocalPort() + request.getContextPath() + "</h2>");
-                out.print("<a href='http://localhost:8081/trolleyes-server/json?ob=usuario&op=login&user=dio&pass=d0c97a588484c47423e5d02db348a5b9cf066825bf44e87a79e0c6aea2cc514e'>Login</a></br>");                   
+                out.print("<a href='http://localhost:8081/trolleyes-server/json?ob=usuario&op=login&user=pablo&pass=26079e41910bcde04be636fbeecc9045379882b5ad3fe7f70b762436c6d98055'>Login</a></br>");                   
                     out.print("<a href='http://localhost:8081/trolleyes-server/json?ob=producto&op=getpage&np=1&rpp=3'>Productos paginados</a></br>");
                     out.print("<a href='http://localhost:8081/trolleyes-server/json?ob=pedido&op=getpage&np=1&rpp=10'>Todos los pedidos</a></br>");
                     out.print("<a href='http://localhost:8081/trolleyes-server/json?ob=carrito&op=add&id=1&cantidad=5'>Añadir al carrito 5 de producto 1</a></br>");
